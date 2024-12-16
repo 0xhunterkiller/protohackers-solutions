98503b8f17c8ba71a746e7be7ddb4aa64d38f986
0xhunterkiller <saaisudarsanan@gmail.com>
2024-12-16 19:42:34 +0530

    data read logic done

--- means-to-an-end/go.mod
+++ means-to-an-end/go.mod
@@ -1,3 +1,5 @@
 module means-to-an-end
 
 go 1.21.13
+
+require github.com/wk8/go-ordered-map v1.0.0 // indirect
--- means-to-an-end/main.go
+++ means-to-an-end/main.go
@@ -3,15 +3,15 @@ package main
 import (
 	"bufio"
 	"encoding/binary"
-	"io"
 	"log"
 	"net"
 )
 
 func handleConnection(conn net.Conn) {
+	raddr := conn.RemoteAddr().String()
+
 	// Close the connection on function exit
 	defer conn.Close()
-	raddr := conn.RemoteAddr().String()
 
 	// Print Remote Address
 	log.Println("Received connection from: ", raddr)
@@ -21,10 +21,10 @@ func handleConnection(conn net.Conn) {
 
 	// Start reading input from the connection
 	for {
-
 		// Get Input
 		var iq bool    // true is insert, false is query
 		fault := false // Set to true if an unexpected behaviour occurs
+
 		op, err := reader.ReadByte()
 		switch op {
 		case 73:
@@ -35,63 +35,44 @@ func handleConnection(conn net.Conn) {
 			fault = true
 		}
 		if err != nil {
-			log.Fatalln("Could not find bytes to read: Terminating Connection with ", raddr)
-			return
+			log.Println("Could not find bytes to read: Terminating Connection with ", raddr)
+			break
 		}
 
 		if fault {
-			log.Fatalln("Unexpected behaviour from client: Terminating Connection with ", raddr)
+			log.Println("Unexpected behaviour from client: Terminating Connection with ", raddr)
 			return
 		}
 
 		// The 2 variables are 32 bits in size and binary.Read() will try to fill it by reading exactly 4 bytes.
 		var x int32
 		err = binary.Read(reader, binary.BigEndian, &x)
-		// If err == nil, then it mean unexpected behaviour
 		if err != nil {
-			if err == io.ErrUnexpectedEOF {
-				log.Fatalln("Error reading bytes (Unexpected EOF): ", err.Error())
-				return
-			}
-			if err != io.EOF {
-				log.Fatalln("Error reading bytes: ", err.Error())
-				return
-			}
-			log.Println("Hit EOF")
-			break
+			log.Println("Unexpected behaviour from client: Terminating Connection ", err.Error())
+			return
 		}
 
 		var y int32
 		err = binary.Read(reader, binary.BigEndian, &y)
-		// If err == nil, then it mean unexpected behaviour
 		if err != nil {
-			if err == io.ErrUnexpectedEOF {
-				log.Fatalln("Error reading bytes (Unexpected EOF): ", err.Error())
-				return
-			}
-			if err != io.EOF {
-				log.Fatalln("Error reading bytes: ", err.Error())
-				return
-			}
-			log.Println("Hit EOF")
-			break
+			log.Println("Unexpected behaviour from client: Terminating Connection ", err.Error())
+			return
 		}
 
 		// Performing Operations
 		if iq {
-			log.Println("Performing Insert Operation")
+			log.Printf("Performing Insert Operation\tTimestamp: %v\tPrice: %v\n", x, y)
 		} else {
-			log.Println("Performing Query Operation")
+			log.Printf("Performing Query Operation\tTimestamp 1: %v\tTimestamp 2: %v\n", x, y)
 		}
 	}
-
 	log.Println("Closing connection with: ", raddr)
 }
 
 func main() {
 	listener, err := net.Listen("tcp", ":2345")
 	if err != nil {
-		log.Fatalln("Error occured while starting server: ", err.Error())
+		log.Println("Error occured while starting server: ", err.Error())
 	}
 	defer listener.Close()
 	log.Println("Serving on port :2345")
@@ -99,7 +80,7 @@ func main() {
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
-			log.Fatalln("Error occured while accepting connection: ", err.Error())
+			log.Println("Error occured while accepting connection: ", err.Error())
 		}
 		go handleConnection(conn)
 	}
